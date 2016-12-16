ac35d24de06aeaf23a9573dfb803150605e15157
bovojon <bovojon@gmail.com>
2016-12-16 15:24:15 -0500

    Connect socket to a server

--- socket.c
+++ socket.c
@@ -20,5 +20,17 @@ int main(int argc, char *argv[]){
     printf("Could not create socket");
   }
 
-  return 0;
+  server.sin_addr .s_addr = inet_addr("74.125.235.20");                       // inet_addr() converts IP address to a long format.
+  server.sin_family = AF_INET;
+  server.sin_port = htons(80);
+
+  // Connect to remote server
+  if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))<0 ){
+    puts("connection error");
+    return 1;
+  }
+
+  puts("Connected");
+  return 0
+
 }
